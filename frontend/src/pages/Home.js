90b18463c57fd2c4acd7999391b492ebf5a4e777
sulamittakauffman <sulamitta.kauffman@hotmail.com.br>
2022-01-06 20:04:44 -0300

    primer commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import MainHome from "../components/MainHome";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <MainHome />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
