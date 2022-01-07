c5c65557542c4f67ccfcb75d93db637ff9779557
haquimjorge <haquimjp@gmail.com>
2022-01-07 09:35:58 -0300

    fixeo bug

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
             </li>
             <li>
               <a className="botones-menu" href="#">
-                Shopp
+                Shop
               </a>
             </li>
             <li>
