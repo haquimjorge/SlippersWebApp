1b39fbcb79b698824e39a5530d93215e18d0cdc7
Kevhausen <kevin.vonhausen@gmail.com>
2022-01-07 11:51:33 -0300

    formulario de signin preliminar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import Home from "./pages/Home";
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/ingresar" element={<SignIn />} exact />
+        <Route path="/registrar" element={<SignUp />} exact />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
