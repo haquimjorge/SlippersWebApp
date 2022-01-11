92e6ce4308e4be97d060089e51b773123f773727
haquimjorge <haquimjp@gmail.com>
2022-01-11 09:06:39 -0300

    m

--- models/User.js
+++ models/User.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, require: true },
-    lastName: { type: String, require: true },
-    email: { type: String, require: true },
-    password:{ type: String, require: true },
-    image:{ type: String, require: true },
-    rol:{ type: String, require: true, default:"Guest" }, // Guest o Admin o Subscriber
-    gender:{ type: String, require: true },
-    favorites: { type: [String], default:[]},
-    googleUser:{type:Boolean, default: false}
-})
+  name: { type: String, require: true },
+  lastName: { type: String, require: true },
+  email: { type: String, require: true },
+  password: { type: String, require: true },
+  image: { type: String, require: true },
+  rol: {
+    type: String,
+    require: true,
+    default: "Guest",
+    enum: ["Guest", "Admin", "Subscriber"],
+  }, // Guest o Admin o Subscriber
+  gender: { type: String, require: true, enum: ["Male", "Female", "Other"] },
+  favorites: { type: [String], default: [] },
+  googleUser: { type: Boolean, default: false },
+});
 
 const User = mongoose.model("user", userSchema);
 
